f76fa05e9de8559feb8d6edf6546d4cf6431acbe
erastoff <yuriy.erastov@yandex.ru>
2022-06-19 19:17:54 +0300

    Functions completed

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,13 +4,14 @@
 """
 
 
-def power_numbers():
+def power_numbers(*args):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    return [item ** 2 for item in args]
 
 
 # filter types
@@ -18,8 +19,18 @@ ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def is_prime(n):
+    i = 2
+    while i*i <= n:
+        if n % i == 0:
+            i += 1
+            return False
+        else:
+            i += 1
+            continue
+    return True
 
-def filter_numbers():
+def filter_numbers(nums, filter):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +41,13 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    if filter == 'odd':
+        return list(filter(lambda x: x % 2 == 0, nums))
+    elif filter == 'even':
+        return list(filter(lambda x: x % 2 != 0, nums))
+    elif filter == 'prime':
+        return list(filter(is_prime, nums))
+    else:
+        print('Unexpected filter!')
+        return None
+
