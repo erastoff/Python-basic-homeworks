335cb6e9ac52b7527383febee936e7a859b41c3a
erastoff <yuriy.erastov@yandex.ru>
2022-09-05 08:01:40 +0300

    jsonplaceholder_requests.py comments added

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -8,6 +8,9 @@ POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
 
 
 async def fetch_json(session: aiohttp.ClientSession, url: str) -> dict:
+    """
+    Function to get json from resource by url.
+    """
     async with session.get(url) as response:  # type aiohttp.ClientResponse
         data = await response.json()
         # print(type(data))
@@ -16,12 +19,18 @@ async def fetch_json(session: aiohttp.ClientSession, url: str) -> dict:
 
 
 async def fetch_users_data(service: str = USERS_DATA_URL) -> list[dict]:
+    """
+    Function to get users data from json.
+    """
     async with aiohttp.ClientSession() as session:
         users: list[dict] = await fetch_json(session, service)
         return users
 
 
 async def fetch_posts_data(service: str = POSTS_DATA_URL) -> list[dict]:
+    """
+    Function to get posts data from json.
+    """
     async with aiohttp.ClientSession() as session:
         posts: list[dict] = await fetch_json(session, service)
         return posts
