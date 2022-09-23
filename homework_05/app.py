cdc7db428ecaa9fce56ec410a9e82da1137ab8bf
erastoff <yuriy.erastov@yandex.ru>
2022-09-23 08:25:12 +0300

    app.py created

--- homework_05/app.py
+++ homework_05/app.py
@@ -10,3 +10,28 @@
 в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
 в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
 """
+
+from flask import Flask, request, render_template
+
+from homework_05.about_app import about_app
+
+app = Flask(__name__)
+
+app.register_blueprint(about_app, url_prefix="/about")
+
+
+def print_request():
+    print(
+        "request: ",
+        request,
+        " | request.method: ",
+        request.method,
+        " | request.path: ",
+        request.path,
+    )
+
+
+@app.route("/", endpoint="index_page")
+def index_view():
+    print_request()
+    return render_template("index.html")
